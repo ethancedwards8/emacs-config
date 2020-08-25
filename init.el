dfd642ad59ef207690a4f9d9171f803679e1c6ea
Ethan Edwards <ethancarteredwards@gmail.com>
2020-08-25 00:52:50 -0400

    trying use-package???

--- init.el
+++ init.el
@@ -1,3 +1,20 @@
+;;; Setting up use-package I think?
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/"))
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+)
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -35,17 +52,11 @@
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
 (setq global-display-line-numbers-mode 'relative)
-;;; My failed relative line numbers attempt
-;; (when (version<= "26.0.50" emacs-version)
-;;   (progn
-;;     (display-line-numbers-mode)
-;;     (setq display-line-numbers-mode 'relative)))
 (powerline-default-theme)
 (setq-default c-basic-offset 8)
 (setq c-default-style '((java-mode . "java")
 			(awk-mode . "awk")
 			(other . "linux")))
-
 ;;(require 'rust-mode)
 ;;(add-hook 'rust-mode-hook
 ;;	  (lambda () (setq indent-tabs-mode nil)))
