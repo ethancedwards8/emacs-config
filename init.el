21a33d6a52aa4c88a3f720bdcf77b93d65c4efeb
Ethan Edwards <ethancarteredwards@gmail.com>
2020-08-27 00:32:35 -0400

    added use-package

--- init.el
+++ init.el
@@ -1,19 +1,9 @@
 ;;; Setting up use-package I think?
-;; (require 'package)
-;; (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-;;                     (not (gnutls-available-p))))
-;;        (proto (if no-ssl "http" "https")))
-;;   (when no-ssl (warn "\
-;; Your version of Emacs does not support SSL connections,
-;; which is unsafe because it allows man-in-the-middle attacks.
-;; There are two things you can do about this warning:
-;; 1. Install an Emacs version that does support SSL and be safe.
-;; 2. Remove this warning from your init file so you won't see it again."))
-;;   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-;;   (add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/"))
-;;   ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;;   ;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
