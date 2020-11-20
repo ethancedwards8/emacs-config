b486dd389d4f455a6f64c9f7c36a3e4a251e8913
Ethan Edwards <ethancarteredwards@gmail.com>
2020-11-20 00:15:54 -0500

    added org-roam and exwm

--- init.el
+++ init.el
@@ -212,6 +212,19 @@ Very Similar to S-o from Vim"
 
   (my/refresh-org-files))
 
+(use-package org-roam
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      (org-roam-directory "~/Nextcloud/Org")
+      :bind (:map org-roam-mode-map
+	      (("C-c n l" . org-roam)
+	       ("C-c n f" . org-roam-find-file)
+	       ("C-c n g" . org-roam-graph))
+	      :map org-mode-map
+	      (("C-c n i" . org-roam-insert))
+	      (("C-c n I" . org-roam-insert-immediate))))
+
 (use-package ox-twbs
   :defer t)
 
