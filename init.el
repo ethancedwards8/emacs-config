cf08486d02a019a6bcdb53eeae1670f211223cb8
Ethan Edwards <ethan@ethancedwards.com>
2021-04-24 18:47:52 -0400

    update

--- init.el
+++ init.el
@@ -103,10 +103,6 @@
 
 (add-hook 'before-save-hook '(lambda () (delete-trailing-whitespace)))
 
-;; (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-;; (global-set-key (kbd "C-r") 'isearch-backward-regexp)
-;; (global-set-key (kbd "C-M-s") 'isearch-forward)
-;; (global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-M-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-M-r") 'isearch-backward-regexp)
 
